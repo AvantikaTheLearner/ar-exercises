c70860fcea0586fd424c59e986302ea8d08530dd
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-11-09 11:54:28 -0500

    Completed exercises 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.name = 'Muskoka'
+@store1.save
+
+#Alternative way to update
+#Store.update(name: 'Muskoka')
+
+@store2 = Store.find_by(id: 2)
+
+pp @store1,@store2
